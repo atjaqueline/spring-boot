a30b986aa230e9f4a1a6c011f5e5c70917350670
Jaqueline Coelho <88630180+jaquelinelin@users.noreply.github.com>
2023-05-15 20:28:32 -0400

    add get request - by id

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -2,13 +2,7 @@ package com.example.springboot;
 
 import com.example.springboot.Repository.MessagesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,17 +25,15 @@ public class HelloController {
 		Message myMessage =  messagesRepository.getReferenceById(id);
 		return myMessage.setMessages();
 	}
+
 	// get all messages
-
-
 	@GetMapping("/messages")
-	public List<Message> getAll(){
+	public List<Message> getAll() {
 		return messagesRepository.findAll();
 	}
+	// adds new message
 	@PostMapping("/messages")
-	 Message messages(@RequestBody Message messages) {
+	Message messages(@RequestBody Message messages) {
 		return messagesRepository.save(messages);
 	}
 }
-
-
--- src/main/java/com/example/springboot/Message.java
+++ src/main/java/com/example/springboot/Message.java
@@ -1,30 +1,31 @@
 package com.example.springboot;
 
-
 import jakarta.persistence.*;
 
 @Entity
 @Table(name = "messages")
 public class Message {
     @Id
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "messages", nullable = false)
     private String messages;
 
-
     public Message() {
     }
-
-    public Message(long id, String messages){
-        this.id = id;
-        this.messages = messages;
-    }
-
+    // setters and getters
     public String getMessages() {
         return messages;
     }
     public String setMessages() {
         return messages;
+    }
+    public Long getId(){
+        return id;
+    }
+    public Long setId(){
+        return id;
     }
 }
