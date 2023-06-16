846c96b590e98f3d5fa7d01127f40620717e8291
Jaqueline Coelho <88630180+jaquelinelin@users.noreply.github.com>
2023-06-15 20:35:52 -0400

    delete messages file

--- build/resources/main/updates-squema.sql
+++ build/resources/main/updates-squema.sql
@@ -13,3 +13,7 @@ CREATE TABLE updates (
     "description": "As you might have expected, every single one of the big banks will be closed on Thanksgiving. You can expect smaller, local banks to be closed as well",
     "submited_by":"furgielicious"
 }
+
+{
+    "content":"nice day"
+}
